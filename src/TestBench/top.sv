7fbe76ed0ce029c21fbc3a3e3e03f85d5ea8d2ad
anjelogigi <anjelogigi@gmail.com>
2026-07-08 22:47:21 +0530

    Update top.sv

--- src/TestBench/top.sv
+++ src/TestBench/top.sv
@@ -1,20 +1,13 @@
-
-
 `include "ram_if.sv"
-
 module top;
-
   import ram_pkg::*;
-
   bit clk;
   bit reset;
-
   initial
   begin
     clk = 0;
     forever #10 clk = ~clk;
   end
-
   initial
   begin
 @(posedge clk)
@@ -23,11 +16,8 @@ module top;
     reset = 0;
     repeat(2) @(posedge clk);
     reset = 1;
-
   end
-
   ram_if intrf(clk, reset);
-
   RAM DUV(
     .data_in(intrf.data_in),
     .write_enb(intrf.write_enb),
@@ -37,13 +27,10 @@ module top;
     .clk(clk),
     .reset(reset)
   );
-
   ram_test tb = new(intrf.DRV, intrf.MON, intrf.REF_SB);
-
   initial
   begin
     tb.run();
     $finish;
   end
-
 endmodule
